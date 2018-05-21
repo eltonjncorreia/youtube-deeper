319d0d142d0f21fda238f8c76c26b1c511422687
elton jefferson <eltonjncorreia@gmail.com>
2018-05-21 10:01:55 -0300

    modelos

--- video/core/models.py
+++ video/core/models.py
@@ -1,3 +1,29 @@
+# from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Theme(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Thumb(models.Model):
+    is_positive = models.BooleanField()
+    time = models.DateTimeField(auto_now_add=True)
+    video = models.ForeignKey('Video', on_delete=models.CASCADE, related_name='thumbs')
+
+
+class Comment(models.Model):
+    is_positive = models.BooleanField()
+    time = models.DateTimeField(auto_now_add=True)
+    video = models.ForeignKey('Video', on_delete=models.CASCADE, related_name='comments')
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    date_uploaded = models.DateTimeField(auto_now_add=False)
+    views = models.IntegerField()
+    # themes = ArrayField(models.CharField(max_length=200), blank=True)
+
+
+
+
