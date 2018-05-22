7ef7dee8c56918660df568e961ceb89089cc4c30
elton jefferson <eltonjncorreia@gmail.com>
2018-05-21 23:20:12 -0300

    views" "

--- video/core/admin.py
+++ video/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from video.core.models import Video, Thumb, Theme, Comment
+
+admin.site.register(Video)
+admin.site.register(Thumb)
+admin.site.register(Theme)
+admin.site.register(Comment)
